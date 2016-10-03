af26e19a37ccf613ce8936a8ffeda85d218a967b
Steven Berlanga <zabawaba99@gmail.com>
2016-10-03 09:26:03 -0400

    Making tests run locally.
    The tests worked if you pointed them at a maven repository. These changes allow someone to run
    go test without modifying the code

--- maven/maven_test.go
+++ maven/maven_test.go
@@ -1,18 +1,43 @@
 package maven
 
-import "testing"
+import (
+	"encoding/xml"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func MetadataMockServer(t *testing.T, groupID, artifactID, version string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		metadata := Metadata{
+			GroupID:    groupID,
+			ArtifactID: artifactID,
+			Versioning: Versioning{
+				Latest:   version,
+				Release:  version,
+				Versions: []string{version},
+			},
+		}
+		if err := xml.NewEncoder(w).Encode(metadata); err != nil {
+			t.Fatal("Failed to marshal xml response", err)
+		}
+	}))
+}
 
 func TestDownloadLatest(t *testing.T) {
 	tests := []struct {
-		repo, group, artifact string
+		repo, group, artifact, version string
 		error
 	}{
-		{repo: "", group: "", artifact: ""},
+		{repo: "", group: "com.foo", artifact: "bar", version: "1.0"},
 	}
 
 	for _, tc := range tests {
-		artifact := NewArtifact(tc.repo, tc.group, tc.artifact)
-		if err := artifact.DownloadLatest(""); err != tc.error {
+		server := MetadataMockServer(t, tc.group, tc.artifact, tc.version)
+		defer server.Close()
+		artifact := NewArtifact(server.URL, tc.group, tc.artifact)
+		if err := artifact.DownloadLatest(os.TempDir() + "/foo"); err != tc.error {
 			t.Errorf("expected: %v, got: %v", tc.error, err)
 		}
 	}
@@ -20,14 +45,16 @@ func TestDownloadLatest(t *testing.T) {
 
 func TestGetMetadata(t *testing.T) {
 	tests := []struct {
-		repo, group, artifact string
+		repo, group, artifact, version string
 		error
 	}{
-		{repo: "", group: "", artifact: ""},
+		{repo: "", group: "com.foo", artifact: "bar", version: "1.0"},
 	}
 
 	for _, tc := range tests {
-		artifact := NewArtifact(tc.repo, tc.group, tc.artifact)
+		server := MetadataMockServer(t, tc.group, tc.artifact, tc.version)
+		defer server.Close()
+		artifact := NewArtifact(server.URL, tc.group, tc.artifact)
 		metadata, err := artifact.GetMetadata()
 		if err != tc.error {
 			t.Errorf("expected: %v, got: %v", tc.error, err)
